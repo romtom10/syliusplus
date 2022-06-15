1e463c479de5ac8ed2fb73ed4a54dd9df9f368c4
djalal <djalal@users.noreply.github.com>
2022-06-15 11:46:06 +0200

    replace lock with main_instance

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -2,64 +2,28 @@
 
 set -e
 
-# source: https://gist.github.com/karlrwjohnson/1921b05c290edb665c238676ef847f3c
-function lock_cmd {
-    LOCK_FILE="$1"; shift
-    LOCK_TIMEOUT="$1"; shift;
+IS_MIGRATED=0
+su www-data -s /bin/bash -c 'php ./bin/console doctrine:migrations:status | grep "Already at latest version"' || IS_MIGRATED=$?
 
-    (
-        trap "rm -f $LOCK_FILE" 0
-        flock -x -w $LOCK_TIMEOUT 200
-        RETVAL=$?
-        if [ $RETVAL -ne 0 ]; then
-            echo -e "Failed to aquire lock on $LOCK_FILE within $LOCK_TIMEOUT seconds. Is a similar script hung?"
-            exit $RETVAL
-        fi
-        echo -e "Running command: $@"
-        $@
-    ) 200>"$LOCK_FILE"
-}
+echo IS_MIGRATED=$IS_MIGRATED
 
-
-echo ">>>>>>>>>>>>>> START CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
-
-# make sure folders are writable
-chown -R www-data:www-data /data
-mkdir -p /var/www/html/var/cache/prod && chown -R www-data:www-data /var/www/html/var/cache/prod
-mkdir -p /var/www/html/var/cache/dev && chown -R www-data:www-data /var/www/html/var/cache/dev
-echo DEBUG 
-ls -la /var/www/html/var/cache
-
-# set runtime env. vars on the fly
-export APP_ENV=prod
-export APP_DATABASE_NAME=${ARTIFAKT_MYSQL_DATABASE_NAME:-changeme}
-export APP_DATABASE_USER=${ARTIFAKT_MYSQL_USER:-changeme}
-export APP_DATABASE_PASSWORD=${ARTIFAKT_MYSQL_PASSWORD:-changeme}
-export APP_DATABASE_HOST=${ARTIFAKT_MYSQL_HOST:-mysql}
-export APP_DATABASE_PORT=${ARTIFAKT_MYSQL_PORT:-3306}
-
-export DATABASE_URL=mysql://$APP_DATABASE_USER:$APP_DATABASE_PASSWORD@$APP_DATABASE_HOST:$APP_DATABASE_PORT/$APP_DATABASE_NAME
-
-# generate jwt data if not present yet
-su www-data -s /bin/bash -c '
-  set -e
-  if [[ ! -f /data/config/jwt/private.pem ]]; then
-    source /data/passphrase
-    jwt_passphrase=${JWT_PASSPHRASE:-$(grep ''^JWT_PASSPHRASE='' .env | cut -f 2 -d ''='')}
-    echo "$jwt_passphrase" | openssl genpkey -out config/jwt/private.pem -pass stdin -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
-    echo "$jwt_passphrase" | openssl pkey -in config/jwt/private.pem -passin stdin -out config/jwt/public.pem -pubout
-    setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
-    setfacl -dR -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
-  fi
-'
-
-wait-for $APP_DATABASE_HOST:$APP_DATABASE_PORT --timeout=180
-
-su www-data -s /bin/bash -c 'php ./bin/console doctrine:migrations:status'
-
-lock_file=/data/artifakt-install-lock
-lock_timeout=600
-
-lock_cmd $lock_file $lock_timeout /.artifakt/install.sh
-
-echo ">>>>>>>>>>>>>> END CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
+if [ $IS_MIGRATED -ne 0 ]; then
+  echo FIRST DEPLOYMENT, RUNNING AUTOMATED INSTALL
+   su www-data -s /bin/sh -c '
+    set -e
+    ls -la /var/www/html/var/cache
+    rm -rf /var/www/html/var/cache/*
+    mkdir -p /var/www/html/public/media/image
+    bin/console sylius:install -s plus -n
+    cp -fr vendor/sylius/plus/src/Resources/templates/bundles/* templates/bundles
+    mkdir -p /var/www/html/templates/bundles/SyliusRefundPlugin
+    mkdir -p /var/www/html/templates/bundles/SyliusUiBundle
+    yarn install
+    yarn build
+    bin/console assets:install --ansi --symlink --relative public
+    bin/console cache:clear
+    bin/console cache:warmup
+  '
+else
+  echo MIGRATIONS DETECTED, SKIPPING AUTOMATED INSTALL
+fi
